43b9849b6aee27b6d8bfa740257b1bd7a6c5bd7a
Keita-lee <keita@barefootbytes.com>
2024-12-02 15:22:39 +0200

    build login page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gmc/LoginPages/login_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+        body: Material(color: Colors.white, child: LoginPage()));
   }
 }
